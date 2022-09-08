be4ef8e5acdf4a15a6a5ed7f765e2c15ae5216ce
Mukthar10 <112944379+Mukthar10@users.noreply.github.com>
2022-09-08 15:20:33 +0530

    Add files via upload

---
+++ Indian Cargo Shipment.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [CargoShip_Details](
+	[Cargo_ID] VARCHAR(5) NOT NULL,
+	[Cargo_Categories] VARCHAR(20) NULL,
+	[Cargo_Capacity] VARCHAR(20) NULL,
+	[Material_Type] VARCHAR(30) NULL,
+	[Container_Details] VARCHAR(6) NOT NULL,
+	[Start_Location] VARCHAR(20) NULL,
+	[End_Location] VARCHAR(20) NULL,
+	PRIMARY KEY(Cargo_ID)) 
+
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR001','Small feeder','Up to 1000','Electronics','CNA001','Mumbai','South Africa')
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR002','Feeder','1001-2000','Steel','CNA002','Kolkata','Brazil')
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR003','Feedermax','2001-3000','Sanatries Products','CNC010','Kandla','Sweden')
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR004','Panamax','3001-5100','Electronics','CND005','Cochin','Canada')
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR005','Post Panamax','5101-10000','Vehicle','CNE007','Panambur','Jubail')
+INSERT [CargoShip_Details]([Cargo_ID],[Cargo_Categories],[Cargo_Capacity],[Material_Type] ,[Container_Details],[Start_Location],[End_Location]) VALUES ('CR006','Neopanamax','10000-14500','Steel and vehicle','CNF008','Ennore','Los Angeles')
+
+
